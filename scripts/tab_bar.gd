51d39c1345d16c6260025c0162462127c04a7a3a
mike7d7 <mike7d7@proton.me>
2025-05-28 09:25:06 -0600

    initial commit

---
+++ scripts/tab_bar.gd
@@ -0,0 +1,12 @@
+extends TabBar
+
+@onready var tab_container_node: Node = get_tree().current_scene.get_node("%BetterTabContainer")
+
+func _ready() -> void:
+	tab_container_node.connect("tab_switched", Callable(self, "on_tab_change"))
+
+func on_tab_change(tab: int):
+	self.current_tab = tab
+
+func _on_tab_clicked(tab: int) -> void:
+	tab_container_node.switch_tab_silent(tab)
---
+++ shell.nix
@@ -0,0 +1,51 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  pkgs = import nixpkgs { 
+    config = {
+      android_sdk.accept_license = true;
+      allowUnfree = true;
+    };
+    overlays = [];
+  };
+in
+
+let
+  androidComposition = pkgs.androidenv.composeAndroidPackages {
+    cmdLineToolsVersion = "13.0";
+    toolsVersion = "26.1.1";
+    platformToolsVersion = "34.0.4";
+    buildToolsVersions = [ "34.0.0" ];
+    includeEmulator = false;
+    emulatorVersion = "30.3.4";
+    platformVersions = [ "34" ];
+    includeSources = false;
+    includeSystemImages = false;
+    systemImageTypes = [ "google_apis_playstore" ];
+    abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+    cmakeVersions = [ "3.10.2" ];
+    includeNDK = true;
+    ndkVersion = "23.2.8568313";
+    useGoogleAPIs = false;
+    useGoogleTVAddOns = false;
+  };
+
+  ndkVersion = androidComposition.ndkVersions.head;
+in
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    godot
+    jdk17
+    pkg-config
+    android-udev-rules
+  ];
+  shellHook = ''
+    alias godot="godot4"
+    export ANDROID_HOME="${androidComposition.androidsdk}/libexec/android-sdk";
+    export ANDROID_NDK_ROOT="$ANDROID_HOME/ndk/23.2.8568313";
+    export ANDROID_SDK_ROOT="$ANDROID_HOME";
+    export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=$ANDROID_HOME/build-tools/34.0.0/aapt2";
+  '';
+}
+
+
