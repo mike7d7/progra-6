b571032ebf460f2a1bd0f34d5e625bba958707bf
mike7d7 <mike7d7@proton.me>
2025-06-02 21:18:51 -0600

    contacto_url.gd: fix var declaration place

--- paginas/contacto_url.gd
+++ paginas/contacto_url.gd
@@ -1,7 +1,7 @@
 extends RichTextLabel
 
 func _richtextlabel_on_meta_clicked(meta):
-	var phone_number: String = "tel:" + str(meta).replace(' ', '')
 	# `meta` is not guaranteed to be a String, so convert it to a String
 	# to avoid script errors at runtime.
+	var phone_number: String = "tel:" + str(meta).replace(' ', '')
 	OS.shell_open(phone_number)
